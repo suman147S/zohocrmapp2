ca7280c190785c569a55d4c4e404a488f6a77445
suman147S <rajs16868@gmail.com>
2023-01-23 10:50:33 +0530

    zohocrmapp2

---
+++ zohocrmapp2/src/main/java/com/zohocrm/entites/Lead.java
@@ -0,0 +1,67 @@
+package com.zohocrm.entites;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "leads")
+public class Lead {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	// here we use snake casing
+	@Column(name= "first_name" , length = 45 , nullable = false)
+	private String firstName;
+	
+	@Column(name= "last_name" , length = 45 , nullable = false)
+	private String lastName;
+	
+	@Column(name= "email" , length = 128 , nullable = false , unique = false)
+	private String email;
+	
+	@Column(name= "mobile" , length = 10 , nullable = false , unique = true)
+	private long mobile;
+	
+	private String source;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+}
